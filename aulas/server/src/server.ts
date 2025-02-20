fbff901790fa08880a3ecc6d2012139884e4fb69
Bruno Rafael <brunrb96@gmail.com>
2025-02-19 22:20:20 -0300

    Configuração, documentação da API

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,41 +1,39 @@
-import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
 import {
-    validatorCompiler,
-    serializerCompiler,
-    ZodTypeProvider,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
 } from 'fastify-type-provider-zod'
-import { z } from 'zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
-
-app.register(fastifyCors)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-// app.get('/hello', () =>{
-//     return 'Hello World'
-// })
-app.post('/subscriptions', {
-    schema:{
-        body: z.object({
-            name: z.string(),
-            email: z.string().email(),
-        }),
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
     },
-},(request, reply) =>{
-    const { name, email } = request.body
-
-
-    // criação da inscrição no banco de dados!
-    return reply.status(201).send({
-        name,
-        email,
-    })
+  },
+  transform: jsonSchemaTransform,
 })
 
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
-app.listen({ port: 3333 }).then(() =>{
-    console.log('HTTP server running!')
+app.register(subscribeToEventRoute)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server running!')
 })
